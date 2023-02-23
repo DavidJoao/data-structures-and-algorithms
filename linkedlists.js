01deffb9aa23ddcac7b4e964b291d26c0c737158
DavidJoao <davidsandoval596@gmail.com>
2023-02-22 21:33:08 -0800

    Create LinkedList and Node class

---
+++ linkedlists.js
@@ -0,0 +1,31 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+const nodeOne = new Node(100)
+const nodeTwo = new Node(200)
+const nodeThree = new Node(300)
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    //Insert First Node
+
+    //Insert Last Node
+
+    //Insert at a specific index
+
+    //Get at specific index
+
+    //Remove specific node by index
+
+    //Clear the list
+
+    //Print all list data
+}
