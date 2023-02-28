1234dc90891a4161d5ecfb2df6cc2eff86183bef
DavidJoao <davidsandoval596@gmail.com>
2023-02-27 19:15:08 -0800

    Starts new LinkedList
    Made it trying to get least help possible

---
+++ linkedlists.js
@@ -0,0 +1,17 @@
+function LinkedList(){
+    let length = 0;
+    let head = null
+
+    let Node = function(element){
+        
+    }
+
+    this.size = function(){
+        return length;
+    }
+
+    this.head = function(){
+        return head;
+    }
+
+}
