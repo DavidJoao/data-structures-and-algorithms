5f6527733535462d7a0782b1fc6509095ac85761
DavidJoao <davidsandoval596@gmail.com>
2023-02-28 20:55:30 -0800

    Create function to add node

--- linkedlists.js
+++ linkedlists.js
@@ -1,9 +1,10 @@
 function LinkedList(){
+    let head = null;
     let length = 0;
-    let head = null
 
-    let Node = function(element){
-        
+    const Node = function(element){
+        this.element = element;
+        this.next = null;
     }
 
     this.size = function(){
@@ -13,5 +14,24 @@ function LinkedList(){
     this.head = function(){
         return head;
     }
+
+    this.add = function(){
+        let node = new Node(element)
+
+        if(head === null){
+            head = node;
+        } else {
+            let currentNode = head;
+
+            while (currentNode.next){
+                currentNode = currentNode.next
+            }
+            currentNode.next = node;
+        }
+
+        length++;
+    }
+
+    
 
 }
