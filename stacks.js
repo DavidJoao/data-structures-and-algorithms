9709b0c93a3988557bc8d9aeca95596334b7377f
DavidJoao <davidsandoval596@gmail.com>
2023-03-01 13:48:25 -0800

    Create pop method without tutorial

--- stacks.js
+++ stacks.js
@@ -8,7 +8,14 @@ let Stack = function(){
         console.log(`${element} pushed to Stack`)
     }
 
+    this.pop = function(){
+        console.log(`${this.items[this.count - 1]} deleted from Stack`)
+        delete this.items[this.count]
+        this.count--;
+    }
 }
 
 let myStack = new Stack
 myStack.push(100)
+myStack.push(300)
+myStack.pop()
