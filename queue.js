0d9c4088e4e1b9d811c5beebb82c655430cb3808
DavidJoao <davidsandoval596@gmail.com>
2023-03-01 14:21:59 -0800

    Create Queue file and initial squeleton

---
+++ queue.js
@@ -0,0 +1,23 @@
+let Queue = function(){
+    this.items = [];
+
+    this.enqueue = function(){
+
+    }
+
+    this.dequeue = function (){
+
+    }
+
+    this.size = function(){
+
+    }
+
+    this.front = function(){
+
+    }
+
+    this.isEmpty = function(){
+        
+    }
+}
