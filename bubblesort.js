e9cbeaa02bb436b38642484475d50012046c43dd
DavidJoao <davidsandoval596@gmail.com>
2023-03-01 21:09:04 -0800

    Create different approach with while loop
    Worked until last number

--- bubblesort.js
+++ bubblesort.js
@@ -13,7 +13,17 @@ const bubbleSort = (array) => {
         }
     }
     console.log(array)
-
 }
+
+// const whileBubbleSort = (array) => {
+//     for (let i = 0; i < array.length * array.length; i++){
+//         while (array[i] > array[i+1]){
+//             let temporal = array[i]
+//             array[i] = array[i+1]
+//             array[i+1] = temporal
+//         }
+//     }
+//     console.log(array)
+// }
 
 bubbleSort(arr)
