353816f69c4aba17c29533daee9b0cd18ca7e80f
DavidJoao <davidsandoval596@gmail.com>
2023-03-02 21:09:02 -0800

    Create selection Sort

--- selectionsort.js
+++ selectionsort.js
@@ -1,4 +1,17 @@
 const randomArray = [1, 6, 4, 8, 12, 2];
 
 const selectionSort = (arr) => {
+
+    for(let i = 0; i < arr.length - 1; i++){
+        let minIndex = i;
+        for(let j = i + 1; j < arr.length; i++){
+            if (arr[j] < arr[minIndex]) minIndex = j
+        }
+        const temp = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+    }
+    return arr
 }
+
+console.log(selectionSort(randomArray))
