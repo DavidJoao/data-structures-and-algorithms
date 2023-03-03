c69328b31390ba18295fff64d351a62fde38d02d
DavidJoao <davidsandoval596@gmail.com>
2023-03-02 21:09:45 -0800

    Fix bug in selection sort function

--- selectionsort.js
+++ selectionsort.js
@@ -4,7 +4,7 @@ const selectionSort = (arr) => {
 
     for(let i = 0; i < arr.length - 1; i++){
         let minIndex = i;
-        for(let j = i + 1; j < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
             if (arr[j] < arr[minIndex]) minIndex = j
         }
         const temp = arr[i]
